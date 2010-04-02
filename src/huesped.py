1cb3c3a58807cac9f962cfd6232acd2797ea4b6a
chiiph <chiiph@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-04-02 23:12:34 +0000

    Update: Huesped para mainwin

--- src/huesped.py
+++ src/huesped.py
@@ -3,17 +3,25 @@ from PyQt4 import QtCore, QtGui
 from ui.huesped import Ui_huespedDialog
 # from db.tipo import SARASA
 
-class huespedDialog(QtGui.QDialog):
+class HuespedDialog(QtGui.QDialog):
 	def setup(self):
 		self.ui = Ui_huespedDialog()
 		self.ui.setupUi(self)
 		
+		self.okBut = self.ui.buttonBox.addButton("Ok", QtGui.QDialogButtonBox.ActionRole)
+		self.cancelBut = self.ui.buttonBox.addButton("Cancel", QtGui.QDialogButtonBox.ActionRole)
+
+		QtCore.QObject.connect(self.okBut, QtCore.SIGNAL("clicked()"),
+				self.on_okBut_clicked)
+		QtCore.QObject.connect(self.cancelBut, QtCore.SIGNAL("clicked()"),
+				self.on_cancelBut_clicked)
+
 		#dni, apellido, nombre, telefono
 	def __init__(self, dni = "", apellido = "", nombre = "", telefono = "", mod = 0, parent = None):
-		super(huespedDialog, self).__init__(parent)
+		super(HuespedDialog, self).__init__(parent)
 		self.setup()
 
-		self.modif = (mod == 0)
+		self.modif = (mod != 0)
 		self.dni = dni
 		self.apellido = apellido
 		self.nombre = nombre
@@ -27,18 +35,30 @@ class huespedDialog(QtGui.QDialog):
 			# db.addNew
 			print "new"
 	
+	def clear(self):
+		self.ui.dniLine.setText("")
+		self.ui.apellidoLine.setText("")
+		self.ui.nombreLine.setText("")
+		self.ui.telLine.setText("")
+
 	@QtCore.pyqtSlot()
-	def on_buttonBox_accepted(self):
+	def on_okBut_clicked(self):
 		self.save()
+		self.clear()
+
+		if self.modif:
+			self.close()
 	
 	@QtCore.pyqtSlot()
-	def on_buttonBox_rejected(self):
-		self.close()
+	def on_cancelBut_clicked(self):
+		self.clear()
+		if self.modif:
+			self.close()
 
-import sys
+#import sys
 
-app = QtGui.QApplication(sys.argv)
-main = huespedDialog()
-main = huespedDialog("dni","apellido","nombre","telefono",1)
-main.show()
-sys.exit(app.exec_())
+#app = QtGui.QApplication(sys.argv)
+#main = huespedDialog()
+#main = huespedDialog("dni","apellido","nombre","telefono",1)
+#main.show()
+#sys.exit(app.exec_())
