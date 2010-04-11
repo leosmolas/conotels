72c64f22b639178b9e2e9e3530d792cacf5e5b29
jjvainstein <jjvainstein@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-04-11 20:28:38 +0000

    Agregue unos mensajes de exito cuando se hacen unas inserciones y ademas mejore lo que se muestra en el table de admin de unidades para que muestre el nombre del tipo de la unidad y no el idTipo de  la misma

--- trunk/src/models/abstractmodel.py
+++ trunk/src/models/abstractmodel.py
@@ -15,7 +15,11 @@ class AbstractModel(object):
 		self.model = self.conn.query("select * from "+self.tableName+" where id="+str(id))
 	
 	def loadAll(self):
-		self.model = self.conn.query("select * from "+self.tableName)
+		
+		if self.tableName == "unidad":
+			self.model = self.conn.query("select unidad.idUnidad,unidad.nombre,tipo.nombre,unidad.capacidad,unidad.descripcion,unidad.estado from unidad,tipo where unidad.tipo = tipo.idTipo")
+		else:
+			self.model = self.conn.query("select * from "+self.tableName)
 	
 	def delete(self, id):
 		self.conn.update("delete from "+self.tableName+" where "+self.id+"="+str(id))
