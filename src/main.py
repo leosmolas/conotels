b2b6e982984bcb7a881e94a75673b403570965ca
leos.molas <leos.molas@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-05-05 19:23:01 +0000

    Modifiqué la ui principal. Espero que les guste ahora :)

--- src/main.py
+++ src/main.py
@@ -19,47 +19,108 @@ class MainWindow(QtGui.QMainWindow):
 	def setup(self):
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
-		self.setStyleSheet("QListView {background-color: transparent;}") #el qt lee css chaboonnnnnnnnnnnnnnnnnnnn es un WIN :D
+		#self.setStyleSheet("QListView {background-color: transparent;}") #el qt lee css chaboonnnnnnnnnnnnnnnnnnnn es un WIN :D
 		self.statusBar = QtGui.QStatusBar(self)
 		self.setStatusBar(self.statusBar)
 		#self.statusBar.showMessage("prueba",3000)
 		self.ui.statusBar = self.statusBar;
 		
-		self.addNewTypeBut = QtGui.QListWidgetItem(self.ui.options)
-		self.addNewTypeBut.setText("Grilla")
-		self.addNewTypeBut.setIcon(QtGui.QIcon(":/calendar.png"))
-		self.addNewTypeBut.setTextAlignment(QtCore.Qt.AlignHCenter)
-		self.addNewTypeBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		self.buttonGroup = QtGui.QButtonGroup(self)
 		
-		self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
-		self.addNewUnitBut.setText("Administrar unidades")
-		self.addNewUnitBut.setIcon(QtGui.QIcon(":/property.png"))
-		self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
-		self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+		self.ui.buttonsLayout.addItem(spacerItem)
+				
+		button = QtGui.QToolButton(self.ui.buttonsFrame)
+		button.setIcon(QtGui.QIcon(":/calendar.png"))
+		button.setText("Grilla")
+		button.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		button.setCheckable(True)
+		button.setChecked(True)
+		self.buttonGroup.addButton(button,1)
+		self.ui.buttonsLayout.addWidget(button)
+		
+		button = QtGui.QToolButton(self.ui.buttonsFrame)
+		button.setIcon(QtGui.QIcon(":/diaryplain.png"))
+		button.setText("Reservas")
+		button.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		button.setCheckable(True)
+		self.buttonGroup.addButton(button,2)
+		self.ui.buttonsLayout.addWidget(button)
+		
+		button = QtGui.QToolButton(self.ui.buttonsFrame)
+		button.setIcon(QtGui.QIcon(":/purse.png"))
+		button.setText("Gastos")
+		button.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		button.setCheckable(True)
+		self.buttonGroup.addButton(button,3)
+		self.ui.buttonsLayout.addWidget(button)
+		
+		button = QtGui.QToolButton(self.ui.buttonsFrame)
+		button.setIcon(QtGui.QIcon(":/people.png"))
+		button.setText("Huésped")
+		button.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		button.setCheckable(True)
+		self.buttonGroup.addButton(button,4)
+		self.ui.buttonsLayout.addWidget(button)
+		
+		button = QtGui.QToolButton(self.ui.buttonsFrame)
+		button.setIcon(QtGui.QIcon(":/property.png"))
+		button.setText("Unidades")
+		button.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		button.setCheckable(True)
+		self.buttonGroup.addButton(button,5)
+		self.ui.buttonsLayout.addWidget(button)
+		
+		button = QtGui.QToolButton(self.ui.buttonsFrame)
+		button.setIcon(QtGui.QIcon(":/tools.png"))
+		button.setText("Tipo")
+		button.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		button.setCheckable(True)
+		self.buttonGroup.addButton(button,6)
+		self.ui.buttonsLayout.addWidget(button)
+		
+		spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+		self.ui.buttonsLayout.addItem(spacerItem)
+		
+		QtCore.QObject.connect(self.buttonGroup, QtCore.SIGNAL("buttonClicked(int)"),
+				self.butClicked)
+		self.butClicked(1)
+		
+		#self.addNewTypeBut = QtGui.QListWidgetItem(self.ui.options)
+		#self.addNewTypeBut.setText("Grilla")
+		#self.addNewTypeBut.setIcon(QtGui.QIcon(":/calendar.png"))
+		#self.addNewTypeBut.setTextAlignment(QtCore.Qt.AlignHCenter)
+		#self.addNewTypeBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		
+		#self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
+		#self.addNewUnitBut.setText("Administrar unidades")
+		#self.addNewUnitBut.setIcon(QtGui.QIcon(":/property.png"))
+		#self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
+		#self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
-		self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
-		self.addNewUnitBut.setText("Administrar reservas")
-		self.addNewUnitBut.setIcon(QtGui.QIcon(":/diaryplain.png"))
-		self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
-		self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		#self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
+		#self.addNewUnitBut.setText("Administrar reservas")
+		#self.addNewUnitBut.setIcon(QtGui.QIcon(":/diaryplain.png"))
+		#self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
+		#self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
-		self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
-		self.addNewUnitBut.setText("Administrar tipo")
-		self.addNewUnitBut.setIcon(QtGui.QIcon(":/tools.png"))
-		self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
-		self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		#self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
+		#self.addNewUnitBut.setText("Administrar tipo")
+		#self.addNewUnitBut.setIcon(QtGui.QIcon(":/tools.png"))
+		#self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
+		#self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
-		self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
-		self.addNewUnitBut.setText("Administrar huesped")
-		self.addNewUnitBut.setIcon(QtGui.QIcon(":/people.png"))
-		self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
-		self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		#self.addNewUnitBut = QtGui.QListWidgetItem(self.ui.options)
+		#self.addNewUnitBut.setText("Administrar huesped")
+		#self.addNewUnitBut.setIcon(QtGui.QIcon(":/people.png"))
+		#self.addNewUnitBut.setTextAlignment(QtCore.Qt.AlignHCenter)
+		#self.addNewUnitBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
-		self.addNewTypeBut = QtGui.QListWidgetItem(self.ui.options)
-		self.addNewTypeBut.setText("Administrar gastos")
-		self.addNewTypeBut.setIcon(QtGui.QIcon(":/purse.png"))
-		self.addNewTypeBut.setTextAlignment(QtCore.Qt.AlignHCenter)
-		self.addNewTypeBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+		#self.addNewTypeBut = QtGui.QListWidgetItem(self.ui.options)
+		#self.addNewTypeBut.setText("Administrar gastos")
+		#self.addNewTypeBut.setIcon(QtGui.QIcon())
+		#self.addNewTypeBut.setTextAlignment(QtCore.Qt.AlignHCenter)
+		#self.addNewTypeBut.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
 
 		#QtCore.QObject.connect(self.ui.menuAdministracion, QtCore.SIGNAL("triggered(QAction *)"),
@@ -73,50 +134,45 @@ class MainWindow(QtGui.QMainWindow):
 
 		self.setup()
 
-		self.ui.options.currentItemChanged.connect(self.changeView)
-		self.ui.options.setViewMode(QtGui.QListView.IconMode)
-		self.ui.options.setCurrentRow(0)
+		#self.ui.options.currentItemChanged.connect(self.changeView)
+		#self.ui.options.setViewMode(QtGui.QListView.IconMode)
+		#self.ui.options.setCurrentRow(0)
 
-	def changeToAction(self, action):
-		self.ui.widgets.removeWidget(self.ui.widgets.currentWidget())
-		if action.text() == "Nueva unidad":
-			self.ui.title.setTitle(action.text())
-			self.ui.widgets.insertWidget(1, UnidadDialog(self.conn))
-		elif action.text() == "Nuevo Tipo":
-			self.ui.title.setTitle(action.text())
-			self.ui.widgets.insertWidget(1, TipoDialog(self.conn))
-		self.ui.widgets.setCurrentIndex(1)
+	#def changeToAction(self, action):
+		#self.ui.widgets.removeWidget(self.ui.widgets.currentWidget())
+		#if action.text() == "Nueva unidad":
+			#self.ui.title.setTitle(action.text())
+			#self.ui.widgets.insertWidget(1, UnidadDialog(self.conn))
+		#elif action.text() == "Nuevo Tipo":
+			#self.ui.title.setTitle(action.text())
+			#self.ui.widgets.insertWidget(1, TipoDialog(self.conn))
+		#self.ui.widgets.setCurrentIndex(1)
 
 	def __del__(self):
 		self.conn.close()
 
-	def changeView(self, current, previous):
-		if not current:
-			current = previous
-
-		selected = self.ui.options.row(current)
-
-		self.ui.widgets.removeWidget(self.ui.widgets.currentWidget())
-		if selected == 0:
-			self.ui.title.setTitle("Grilla")
-			self.ui.widgets.insertWidget(1, GrillaDialog(self.conn))
-		elif selected == 1:
-			self.ui.title.setTitle("Administrar unidades")
-			self.ui.widgets.insertWidget(1, Admin(self.conn, "Unidad",self.ui))
-		elif selected == 2:
-			self.ui.title.setTitle("Administrar reservas")
-			self.ui.widgets.insertWidget(1, Admin(self.conn, "Reserva",self.ui))
-		elif selected == 3:
-			self.ui.title.setTitle("Administrar tipos")
-			self.ui.widgets.insertWidget(1, Admin(self.conn, "Tipo",self.ui))
-		elif selected == 4:
-			self.ui.title.setTitle("Administrar huespedes")
-			self.ui.widgets.insertWidget(1, Admin(self.conn, "Huesped",self.ui))
-		elif selected == 5:
-			self.ui.title.setTitle("Gastos")
-			self.ui.widgets.insertWidget(1, GastosDialog(self.conn,self))
-
-		self.ui.widgets.setCurrentIndex(1)
+	def butClicked(self, selected):
+		if selected == self.buttonGroup.checkedId():
+			self.ui.widgets.removeWidget(self.ui.widgets.currentWidget())
+			if selected == 1:
+				self.ui.title.setTitle("Grilla")
+				self.ui.widgets.insertWidget(1, GrillaDialog(self.conn))
+			elif selected == 2:
+				self.ui.title.setTitle("Reservas")
+				self.ui.widgets.insertWidget(1, Admin(self.conn, "Reserva",self.ui))
+			elif selected == 3:
+				self.ui.title.setTitle("Gastos")
+				self.ui.widgets.insertWidget(1, GastosDialog(self.conn,self))
+			elif selected == 4:
+				self.ui.title.setTitle("Huespedes")
+				self.ui.widgets.insertWidget(1, Admin(self.conn, "Huesped",self.ui))
+			elif selected == 5:
+				self.ui.title.setTitle("Unidades")
+				self.ui.widgets.insertWidget(1, Admin(self.conn, "Unidad",self.ui))
+			elif selected == 6:
+				self.ui.title.setTitle("Tipos")
+				self.ui.widgets.insertWidget(1, Admin(self.conn, "Tipo",self.ui))
+			self.ui.widgets.setCurrentIndex(1)
 
 import sys
 
