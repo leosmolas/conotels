5b724fc725d25493eef858b5978b71a2ef8d1528
leos.molas <leos.molas@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-05-05 19:23:01 +0000

    Modifiqué la ui principal. Espero que les guste ahora :)

--- trunk/src/ui/mainwin.py
+++ trunk/src/ui/mainwin.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwin.ui'
+# Form implementation generated from reading ui file 'ui/mainwin.ui'
 #
-# Created: Tue May 04 19:57:05 2010
-#      by: PyQt4 UI code generator 4.7.3
+# Created: Wed May  5 16:19:56 2010
+#      by: PyQt4 UI code generator 4.6.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,7 +12,7 @@ from PyQt4 import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(589, 771)
+        MainWindow.resize(566, 822)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_2 = QtGui.QGridLayout(self.centralwidget)
@@ -21,10 +21,19 @@ class Ui_MainWindow(object):
         self.gridLayout.setObjectName("gridLayout")
         self.title = QtGui.QGroupBox(self.centralwidget)
         self.title.setObjectName("title")
-        self.gridLayout_3 = QtGui.QGridLayout(self.title)
-        self.gridLayout_3.setVerticalSpacing(2)
-        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.title)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.buttonsFrame = QtGui.QFrame(self.title)
+        self.buttonsFrame.setObjectName("buttonsFrame")
+        self.buttonsLayout = QtGui.QHBoxLayout(self.buttonsFrame)
+        self.buttonsLayout.setObjectName("buttonsLayout")
+        self.verticalLayout_3.addWidget(self.buttonsFrame)
         self.widgets = QtGui.QStackedWidget(self.title)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widgets.sizePolicy().hasHeightForWidth())
+        self.widgets.setSizePolicy(sizePolicy)
         self.widgets.setObjectName("widgets")
         self.page = QtGui.QWidget()
         self.page.setObjectName("page")
@@ -41,30 +50,7 @@ class Ui_MainWindow(object):
         self.page_2 = QtGui.QWidget()
         self.page_2.setObjectName("page_2")
         self.widgets.addWidget(self.page_2)
-        self.gridLayout_3.addWidget(self.widgets, 2, 0, 1, 5)
-        spacerItem = QtGui.QSpacerItem(0, 0, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
-        self.gridLayout_3.addItem(spacerItem, 1, 3, 1, 2)
-        spacerItem1 = QtGui.QSpacerItem(0, 0, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
-        self.gridLayout_3.addItem(spacerItem1, 1, 0, 1, 2)
-        self.options = QtGui.QListWidget(self.title)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.options.sizePolicy().hasHeightForWidth())
-        self.options.setSizePolicy(sizePolicy)
-        self.options.setMaximumSize(QtCore.QSize(440, 105))
-        self.options.setFrameShape(QtGui.QFrame.NoFrame)
-        self.options.setFrameShadow(QtGui.QFrame.Sunken)
-        self.options.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.options.setIconSize(QtCore.QSize(60, 60))
-        self.options.setMovement(QtGui.QListView.Static)
-        self.options.setFlow(QtGui.QListView.LeftToRight)
-        self.options.setSpacing(10)
-        self.options.setViewMode(QtGui.QListView.IconMode)
-        self.options.setUniformItemSizes(True)
-        self.options.setWordWrap(True)
-        self.options.setObjectName("options")
-        self.gridLayout_3.addWidget(self.options, 1, 2, 1, 1)
+        self.verticalLayout_3.addWidget(self.widgets)
         self.gridLayout.addWidget(self.title, 0, 1, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 1, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -83,12 +69,24 @@ class Ui_MainWindow(object):
         self.actionAyuda.setObjectName("actionAyuda")
 
         self.retranslateUi(MainWindow)
-        self.widgets.setCurrentIndex(0)
+        self.widgets.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
         self.title.setTitle(QtGui.QApplication.translate("MainWindow", "GroupBox", None, QtGui.QApplication.UnicodeUTF8))
+        self.buttonsFrame.setStyleSheet(QtGui.QApplication.translate("MainWindow", "QToolButton{\n"
+"    border: 0px;    \n"
+"    icon-size:64px ;\n"
+"    text-align:center;\n"
+"}\n"
+"\n"
+"QToolButton:checked{\n"
+"    border-radius: 2px;\n"
+"    background-color: rgba(125, 125, 125, 100);\n"
+"}\n"
+"\n"
+"", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("MainWindow", " Bienvenido!", None, QtGui.QApplication.UnicodeUTF8))
         self.actionNueva_unidad.setText(QtGui.QApplication.translate("MainWindow", "Nueva unidad", None, QtGui.QApplication.UnicodeUTF8))
         self.actionNuevo_Tipo.setText(QtGui.QApplication.translate("MainWindow", "Nuevo Tipo", None, QtGui.QApplication.UnicodeUTF8))
