b23f405314461150dd4125ed2fc56cc5ed000ca3
chiiph <chiiph@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-05-06 23:57:42 +0000

    Test 3

--- trunk/src/grilla.py
+++ trunk/src/grilla.py
@@ -101,7 +101,7 @@ class GrillaDialog(QtGui.QDialog):
 		while date.month() == mes:
 			days+=1
 			date = date.addDays(1)
-			print str(date.longDayName(date.dayOfWeek())).encode("iso8859-1")
+			print date.shortDayName(date.dayOfWeek())
 
 		self.ui.tableWidget.setColumnCount(days)
 		self.ui.tableWidget.resizeColumnsToContents()
