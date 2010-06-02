e4a44eed8141d5cc360a08d5ba6e40c7bdb83629
fsisul <fsisul@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-06-02 07:38:36 +0000

    Modifique la base de datos para que la tabla RESERVA tenga un cambo GASTOS (float unsigned default 0) para guardar todos los gastos de la reserva. Ademas arme 2 triggers. Uno para cuando se INSERTA un gasto, se actualice la variable gastos de la tabla reservas a la cual se inserto el gasto. El otro para cuando se hace un UPDATE de un gasto, tambien se actualice la variable gastos de la reserva en cuestion.
    Si tienen la base de datos con datos y no quieren perderla usen updatetrigger.sql.... sino usen el drop y conotels.sql que ya le puse los triggers a este.

--- trunk/src/drop.sql
+++ trunk/src/drop.sql
@@ -1,4 +1,6 @@
 use conotels;
 drop user userHoteles;
 drop trigger update_unidad;
+drop trigger update_gasto;
+drop trigger insert_gasto;
 drop database conotels;
---
+++ trunk/src/updatetrigger.sql
@@ -0,0 +1,24 @@
+use conotels
+
+alter table reserva add gastos float unsigned default 0;
+
+update reserva set gastos = 0;
+
+delimiter //
+create trigger insert_gasto after insert on gasto
+	for each row begin
+		declare valor float;
+		set valor = new.costo;
+		update reserva set gastos = gastos + valor where idReserva = new.reserva;
+	end//
+delimiter ;
+
+delimiter //
+create trigger update_gasto before update on gasto
+	for each row begin
+		declare valor float;
+		set valor = new.costo - old.costo;
+		update reserva set gastos = gastos + valor where idReserva = new.reserva;
+	end//
+delimiter ;
+
