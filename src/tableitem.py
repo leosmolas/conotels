0615a686916c8ddf82554236eb34eed452e13f49
chiiph <chiiph@949b7596-18b6-11df-9eea-1f1cb9bda66b>
2010-06-30 22:25:13 +0000

    Agrega el tooltip

--- src/tableitem.py
+++ src/tableitem.py
@@ -8,7 +8,7 @@ from PyQt4 import QtCore,QtGui
 # 3 - Reserva en curso
 
 class TableItem(QtGui.QTableWidgetItem):
-	def __init__(self, reserva = -1, tipo = 0, init = False):
+	def __init__(self, conn, reserva = -1, tipo = 0, init = False):
 		super(TableItem, self).__init__()
 
 		if init:
@@ -16,6 +16,11 @@ class TableItem(QtGui.QTableWidgetItem):
 
 		self.reserva = reserva
 		self.tipo = tipo
+	
+		if(reserva != -1):
+			self.model = conn.query("select huesped.nombre, huesped.apellido, reserva.gastos from huesped, reserva where huesped.idHuesped = reserva.huesped and reserva.idReserva = " + str(reserva))
+			toolTip = u"Huesped: " + self.model.getItem(0, 0).toString() + " " + self.model.getItem(1, 0).toString() + "\nGastos: " + str(self.model.getItem(2, 0).toInt()[0])
+			self.setToolTip(toolTip)	
 
 		color = QtGui.QColor(255,255,255)
 	
